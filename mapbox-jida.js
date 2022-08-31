595c078e738fff76e265a7188309d25a6ccdc448
Shane M. Ross <shanedroid23@gmail.com>
2022-08-31 13:18:55 -0500

    not console logging

--- mapbox-jida.js
+++ mapbox-jida.js
@@ -568,7 +568,7 @@ function setActiveChapter(chapterName) {
 
 function isElementOnScreen(id) {
   var element = document.getElementById(id);
-  console.log(element)
+  // console.log(element)
   var bounds = element.getBoundingClientRect();
   return bounds.top < window.innerHeight && bounds.bottom > 300;
 }
